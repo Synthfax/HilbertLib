89b1002e1651be094f5106d75a0323c7723c0b61
Synthfax <synthfax@gmail.com>
2025-06-19 23:25:18 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hilbertlib',
-    version='6.19.25',
+    version='6.19.25.1',
     description='HilbertLib is a Python library providing modular tools for bot development, mathematical operations, web utilities, color manipulation, and database management.',
     author='Synthfax',
     author_email='synthfax@gmail.com',
@@ -15,8 +15,11 @@ setup(
     ],
     python_requires='>=3.7',
     url='https://github.com/Synthfax/HilbertLib',
+    license='MIT',
+    license_files=('LICENSE',),
     classifiers=[
         'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent'
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
     ],
 )
