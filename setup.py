1e034d5342ecd043bfcadc3e5c5ae7d353e92525
Synthfax <synthfax@gmail.com>
2025-06-21 22:08:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name='hilbertlib',
-    version='6.19.25.1',
+    version='6.19.25.2',
     description='HilbertLib is a Python library providing modular tools for bot development, mathematical operations, web utilities, color manipulation, and database management.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Synthfax',
     author_email='synthfax@gmail.com',
     packages=find_packages(),
